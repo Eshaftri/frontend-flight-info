16d21e14d14a6ab8a5c4854079f717832315450d
Mohamed Eshaftri <eshaftri@gmail.com>
2019-10-23 18:31:39 +0100

    fix table view' q exit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import NavComp from "./components/NavComp";
-import SearchComp from "./components/SearchComp";
-import FlightStyle from "./components/FlightList";
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import NavComp from "./components/NavComp"
+import SearchComp from "./components/SearchComp"
+import FlightStyle from "./components/FlightList"
 
-class App extends React.Component() {
+class App extends React.Component{
+  
+  state = {
+    data: [],
+    query : ''
+  }
+
+  componentDidMount(arrDep,flightNo) {
+    const url = "https://kabrudle.edinburghairport.com/api/flights/all";
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        data => this.setState({ data: data.filter(
+        data => data.ArrDep === arrDep
+      )})
+      );
+  }
+
+  handleArrDep = arrDep => {
+    this.setState({query : ''})
+    this.componentDidMount(arrDep)
+  }
+
   render() {
+    let filterFlight  = this.state.data.filter((flight)=>{
+      return flight.FlightNo.includes(this.state.query) || flight.Status.includes(this.state.query)
+  })
     return (
       <React.Fragment>
         <NavComp />
         <SearchComp />
-        <FlightStyle />
+        <FlightStyle data = {this.state.data} filterFlight = {filterFlight}/>
       </React.Fragment>
     );
   }
