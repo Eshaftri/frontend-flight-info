e8b8cd0d8f470c8a0819f2a9ea853d07064e851b
Mohamed Eshaftri <eshaftri@gmail.com>
2019-10-23 18:13:51 +0100

    change app fun component to class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavComp from './components/NavComp';
-import SearchComp from './components/SearchComp';
-import FlightStyle from './components/FlightList'
+import NavComp from "./components/NavComp";
+import SearchComp from "./components/SearchComp";
+import FlightStyle from "./components/FlightList";
 
-function App() {
-  return (
-    <React.Fragment>
-    <NavComp/>
-    <SearchComp/>
-    <FlightStyle/>
-    </React.Fragment>
-  );
+class App extends React.Component() {
+  render() {
+    return (
+      <React.Fragment>
+        <NavComp />
+        <SearchComp />
+        <FlightStyle />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
