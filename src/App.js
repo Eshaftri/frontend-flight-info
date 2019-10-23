9f97d052248ae4f83e5f2785b9fa62881beb8676
Mohamed Eshaftri <eshaftri@gmail.com>
2019-10-23 17:59:44 +0100

    create Search Component and add two input field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import NavComp from './components/NavComp'
+import SearchComp from './components/SearchComp'
 
 function App() {
   return (
     <React.Fragment>
     <NavComp/>
+    <SearchComp/>
     </React.Fragment>
   );
 }
---
+++ src/components/SearchComp.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./SearchComp.css";
+
+const SearchComp = props => (
+  <React.Fragment>
+    <div className="container search_bar">
+      <div className="row justify-content-center align-items-center ">
+        <div className="col-md-6">
+            <div className="input-group mt-4">
+              <input
+                type="text"
+                name="search"
+                placeholder="Search by flight number..."
+                className="form-control"
+                onChange={props.handleSearch}
+              />
+            </div>
+        </div>
+        <div className="col-md-6 my-col">
+          <div className="input-group mt-4 ">
+            <div className="input-group-prepend">
+              <label className="input-group-text" >
+                Filter
+              </label>
+            </div>
+            <select className="custom-select" defaultValue='' onChange ={props.handleSelect}>
+              <option value ="" >All...</option>
+              <option value="EXPECTED">EXPECTED</option>
+              <option value="SCHEDULED">SCHEDULED</option>
+              <option value="LANDED">LANDED</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  </React.Fragment>
+);
+
+export default SearchComp;
